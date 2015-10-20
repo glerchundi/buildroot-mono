2a90f255d18b5dfaa81636ab91ee7a17d85fd4b2
Gorka Lerchundi Osa <glertxundi@gmail.com>
2015-10-20 17:51:42 +0200

    fix abs. path issues with cross-compiled mono build

--- postbuild.sh
+++ postbuild.sh
@@ -3,6 +3,11 @@ set -x
 
 [ -n "${TARGET_DIR}" ] || exit
 
+# fix issue with absolute paths, until something more is done with this:
+# http://article.gmane.org/gmane.comp.lib.uclibc.buildroot/129991
+find ${TARGET_DIR}/etc/mono -type f -print0 | xargs -0 sed -i 's/\/home\/travis\/buildroot\/output\/host//g'
+find ${TARGET_DIR}/usr/lib/mono -type f -print0 | xargs -0 sed -i 's/\/home\/travis\/buildroot\/output\/host//g'
+
 # delete not needed file/folders
 cd "${TARGET_DIR}"
 rm -rf root/.bash_* home/ftp
