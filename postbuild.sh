4c14c8908001e2fac053a35d0a9624e49a17c5d9
Gorka Lerchundi Osa <glertxundi@gmail.com>
2015-10-22 10:14:11 +0200

    sync with mozroots certificates

--- postbuild.sh
+++ postbuild.sh
@@ -3,14 +3,16 @@ set -x
 
 [ -n "${TARGET_DIR}" ] || exit
 
-# fix issue with absolute paths, until something more is done with this:
-# http://article.gmane.org/gmane.comp.lib.uclibc.buildroot/129991
-find ${TARGET_DIR}/etc/mono -type f -print0 | xargs -0 sed -i 's/\/home\/travis\/buildroot\/output\/host//g'
-find ${TARGET_DIR}/usr/lib/mono -type f -print0 | xargs -0 sed -i 's/\/home\/travis\/buildroot\/output\/host//g'
+# move to target folder
+cd "${TARGET_DIR}"
 
 # delete not needed file/folders
-cd "${TARGET_DIR}"
 rm -rf root/.bash_* home/ftp
+
+# fix issue with absolute paths, until something more is done with this:
+# http://article.gmane.org/gmane.comp.lib.uclibc.buildroot/129991
+find etc/mono -type f -print0 | xargs -0 sed -i 's/\/home\/travis\/buildroot\/output\/host//g'
+find usr/lib/mono -type f -print0 | xargs -0 sed -i 's/\/home\/travis\/buildroot\/output\/host//g'
 
 # create required folders
 mkdir -p etc/mono/registry/LocalMachine
